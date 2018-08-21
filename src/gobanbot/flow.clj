789570fd012c2499249ade6dfbf6406e8dbd919e
Vlad Synkov <himself@vladsynkov.ru>
2018-08-21 16:18:56 +0300

    Add logging

--- src/gobanbot/flow.clj
+++ src/gobanbot/flow.clj
@@ -1,6 +1,7 @@
 (ns gobanbot.flow
   (:require [clojure.string :as s]
             [clojure.set :as ss]
+            [clojure.tools.logging :as log]
             [gobanbot.storage :as storage]))
 
 (defn moves-to-mvc [moves] (zipmap (map :mv moves) (map :color moves)))
@@ -11,11 +12,11 @@
 (defn is-move? [size value]
   (let [max-letter (-> size (+ 96) char str)]
   (if-not (empty? value)
-    (re-find (re-pattern 
-               (str 
-                 "^[a-" 
-                 max-letter 
-                 "]{2}$|^pass$|^resigni$")) 
+    (re-find (re-pattern
+               (str
+                 "^[a-"
+                 max-letter
+                 "]{2}$|^pass$|^resigni$"))
              value))))
 
 (defn next-bwid [{:keys [bid wid handicap]} uid]
@@ -28,14 +29,14 @@
             (cond (not wid) :wid
                   (not bid) :bid))))
 
-(defn decide-move 
+(defn decide-move
   [{:keys [moves bid wid ended started] :as game} uid mv]
   (let [bwid (next-bwid game uid)
         color (cond (= bwid :bid) "b" (= bwid :wid) "w")]
     {:bwid bwid
      :color color
      :status
-     (cond 
+     (cond
        (not game) :no-game
        (not color) :not-a-player
        (= started 0) :not-started
@@ -46,22 +47,22 @@
 
 (defn should-end? [game]
   (or (->> game :moves (some #(= "resign" (:mv %))))
-      (->> game 
+      (->> game
            :moves
-           (filter (comp (partial = "pass") :mv)) 
-           count 
+           (filter (comp (partial = "pass") :mv))
+           count
            (= 2))))
 
 (defn show [{:keys [size moves]}]
   (let [mvc (moves-to-mvc moves)]
-    (str 
+    (str
       (apply str (repeat (+ 2 size) "-")) "\n"
       (apply str (for [v (range size)]
         (str "|"
-           (apply str (for [h (range size)] 
-                        (case (mvc (str (-> h (+ 97) char) (-> v (+ 97) char))) 
-                          "b" "x" 
-                          "w" "o" 
+           (apply str (for [h (range size)]
+                        (case (mvc (str (-> h (+ 97) char) (-> v (+ 97) char)))
+                          "b" "x"
+                          "w" "o"
                           nil "+")))
            "|\n")))
       (apply str (repeat (+ 2 size) "-")))))
@@ -76,27 +77,27 @@
 
 (defn get-group-at
   ([{:keys [size moves]} color mv]
-    (get-group-at size 
-                  (moves-to-mvc moves) 
-                  color 
-                  #{} 
+    (get-group-at size
+                  (moves-to-mvc moves)
+                  color
+                  #{}
                   mv))
 
   ([size mvc color found mv]
   (if (= (mvc mv) color)
-    (conj 
-      (apply ss/union (map 
+    (conj
+      (apply ss/union (map
         #(get-group-at
-           size 
+           size
            mvc
-           color 
-           (conj found mv) 
+           color
+           (conj found mv)
            %)
         (-> mv (get-near-cells size) set (ss/difference found))))
       mv)
     found)))
 
-(defn get-dame [{:keys [size moves]} group] 
+(defn get-dame [{:keys [size moves]} group]
   (let [mvc (moves-to-mvc moves)]
   (->> group
       (map #(get-near-cells % size))
@@ -105,7 +106,7 @@
       (filter (comp not (partial get mvc)))
       count)))
 
-(defn find-to-eat [{:keys [size moves] :as game} color mv] 
+(defn find-to-eat [{:keys [size moves] :as game} color mv]
   (let [op-color (if (= color "b") "w" "b")]
     (->> mv
          (#(get-near-cells % size))
@@ -119,15 +120,15 @@
   (storage/insert-move! game color mv)
   (let [gid (:gid game)
         updated (storage/get-game gid)]
-    (if (should-end? updated) 
+    (if (should-end? updated)
       (storage/end-game! gid)
       (doall (map storage/mark-eaten! (find-to-eat updated color mv))))))
 
 (defn move! [game uid mv]
-  (println "move gid" (:gid game) uid mv)
+  (log/debug "move gid" (:gid game) uid mv)
   (let [gid (:gid game)
         {:keys [status color bwid] :as stat} (decide-move game uid mv)]
-    (println "move status" stat)
+    (log/debug "move status" stat)
     (if bwid (storage/set-player! gid bwid uid))
     (if (= status :ok) (add-move! game color mv))
     status))
